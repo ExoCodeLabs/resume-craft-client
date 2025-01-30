48b7018cea95552def58f91efc3d984d7036e51c
Rishi <developer.rishi.99@gmail.com>
2025-01-30 13:49:24 -0500

    -- added pages config

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath: "/resume-craft-client",
+  images: {
+    unoptimized: true,
+  },
   experimental: {
     serverComponentsExternalPackages: ["pdf-parse", "fs"], // Treat pdf-parse as a server-side package
   },
